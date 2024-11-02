fc2642164152c0c4db6bb61690575b048c0e1e75
Luc4sf4 <superlucas2005@gmail.com>
2024-11-02 15:33:54 -0300

    Adding bottom pipes

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -99,9 +99,15 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         // 128
         //0 - 128 -(0- 256) -->  pipeHeight /4 -> 3/4 pipeHeight
         int randomPipeY = (int) (pipeY - pipeHeight / 4 - Math.random() * (pipeHeight /2));
+        int openingSpace = boardHeight / 4;
+
         Pipe topPipe = new Pipe(topPipeImg);
         topPipe.y = randomPipeY;
         pipes.add(topPipe);
+
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
+        pipes.add(bottomPipe);
     }
 
 
